81c0770c933b5b9f449f8d772650a83814862a20
bernardomecabo <bernardomecabo09@gmail.com>
2025-06-25 22:21:57 -0300

    TransactionService added

---
+++ src/main/java/com/bernardomecabo/financial_control_system/service/TransactionService.java
@@ -0,0 +1,24 @@
+package com.bernardomecabo.financial_control_system.service;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bernardomecabo.financial_control_system.model.Transaction;
+import com.bernardomecabo.financial_control_system.repo.TransactionRepo;
+
+@Service
+public class TransactionService {
+    @Autowired
+    private TransactionRepo transactionRepo;
+
+    public BigDecimal getBalance(Long userId){
+        return transactionRepo.calcularSaldo(userId);
+    }
+
+    public List<Transaction> getTransactionsByUserId(Long userId){
+        return transactionRepo.findByUserId(userId);
+    }
+}
